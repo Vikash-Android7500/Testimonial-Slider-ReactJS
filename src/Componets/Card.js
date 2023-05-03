ab10f8c1843e89fb42f1902efb8f5df95a06b8d6
Vikash-Android7500 <Vikash75.jm@gmail.com>
2023-05-03 15:12:12 +0530

    props change in CardJS file

--- src/Componets/Card.js
+++ src/Componets/Card.js
@@ -1,33 +1,34 @@
 import React from "react";
 import {FaQuoteLeft, FaQuoteRight} from "react-icons/fa";
 
-const Card = (props) => {
-     let review = props.review;
+const Card = ({props, review}) => {
+     const {image, name, job, text} = review;
+
      return (
        <div className="flex flex-col md:relative">
          {/* images */}
          <div className="absolute top-[-7rem] z-10 mx-auto">
            <img
              className="w-[140px] h-[140px] aspect-square rounded-full z-[1]"
-             src={review.image}
+             src={image}
            />
            <div className="w-[140px] h-[140px] bg-violet-500 rounded-full absolute top-[-6px] left-2 z-[-1]"></div>
          </div>
 
          {/* Names */}
          <div className="text-center mt-7">
-           <p className="font-bold text-2xl capitalize">{review.name}</p>
+           <p className="font-bold text-2xl capitalize">{name}</p>
          </div>
 
          <div className="text-center mt-1">
-           <p className="text-violet-300 uppercase text-sm">{review.job}</p>
+           <p className="text-violet-300 uppercase text-sm">{job}</p>
          </div>
 
          <div className="mx-auto mt-5 text-violet-400">
            <FaQuoteLeft />
          </div>
 
-         <div className="text-center mt-4 text-slate-500">{review.text}</div>
+         <div className="text-center mt-4 text-slate-500">{text}</div>
 
          <div className="mx-auto mt-5 text-violet-400">
            <FaQuoteRight />
--- src/Componets/Testimonials.js
+++ src/Componets/Testimonials.js
@@ -3,6 +3,7 @@ import Card from "./Card";
 import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
 import { useState } from "react";
 const Testimonials = (props) => {
+  
      let reviews = props.reviews;
      const [index, setIndex] = useState(0);
 
